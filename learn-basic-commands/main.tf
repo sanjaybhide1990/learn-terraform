e32015d27aa9302b0fc50c71f71a00d2f884cb37
sanjay.bhide <sanjay.bhide@cafu.com>
2026-01-17 16:20:47 +0530

    Files created using count

--- learn-basic-commands/main.tf
+++ learn-basic-commands/main.tf
@@ -1,15 +1,20 @@
 resource "local_file" "hello_world" {
-#   filename = "${var.path}/${var.filename}"
-filename = "${var.filename}"
-  content = "Message: ${var.message}"
+  filename = var.filename
+  content  = "Message: ${var.message}"
+}
+
+resource "local_file" "file_created_using_count" {
+    filename = var.filenames[count.index]
+    count = length(var.filenames)
+    content = "Message: File is created using count meta argument"
 }
 
 output "created-filename" {
   description = "Displays the name of the created file"
-  value = local_file.hello_world.filename
+  value       = local_file.hello_world.filename
 }
 
 output "message-added" {
   description = "Displays the message added to the file"
-  value = local_file.hello_world.content
+  value       = local_file.hello_world.content
 }
--- learn-basic-commands/variables.tf
+++ learn-basic-commands/variables.tf
@@ -1,17 +1,29 @@
 variable "filename" {
-  type = string
+  type        = string
   description = "Contains the filename"
-  default = "test-file"
+  default     = "test-file"
+}
+
+variable "filenames" {
+  type        = list(string)
+  description = "Contains the list of file names to be created"
+  default     = ["file-1", "file-2", "file-3"]
+}
+
+variable "messages" {
+  type        = list(string)
+  description = "Contains the list of messages to be added"
+  default     = ["message-1", "message-2", "message-3"]
 }
 
 variable "message" {
-  type = string
+  type        = string
   description = "Contains the message"
-  default = "Hello World!"
+  default     = "Hello World!"
 }
 
 variable "path" {
-  type = string
+  type        = string
   description = "Contains the path"
-  default = "/Users/sanjaybhide/Desktop/development/learn-terraform/learn-basic-commands"
+  default     = "/Users/sanjaybhide/Desktop/development/learn-terraform/learn-basic-commands"
 }
