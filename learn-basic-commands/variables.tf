4fcf9f9203c1ffeddff07b84eaa582f5c7526ce0
sanjay.bhide <sanjay.bhide@cafu.com>
2026-01-17 15:48:39 +0530

    Updated the variables file

---
+++ learn-basic-commands/variables.tf
@@ -0,0 +1,17 @@
+variable "filename" {
+  type = string
+  description = "Contains the filename"
+  default = "test-file"
+}
+
+variable "message" {
+  type = string
+  description = "Contains the message"
+  default = "Hello World!"
+}
+
+variable "path" {
+  type = string
+  description = "Contains the path"
+  default = "/Users/sanjaybhide/Desktop/development/learn-terraform/learn-basic-commands"
+}
